6bfebb8a7524ee44efe64e0095cf2d86f5a59582
gos-k <mag4.elan@gmail.com>
2015-09-20 02:48:37 +0900

    add primitive types.

--- src/cl-oclapi.lisp
+++ src/cl-oclapi.lisp
@@ -7,3 +7,21 @@
   (t (:default "libOpenCL")))
 
 (use-foreign-library libopencl)
+
+#| cl-platform.h |#
+(defctype cl-char :int8)
+(defctype cl-uchar :uint8)
+(defctype cl-short :int16)
+(defctype cl-ushort :uint16)
+(defctype cl-int :int32)
+(defctype cl-uint :uint32)
+(defctype cl-long :int64)
+(defctype cl-ulong :uint64)
+
+(defctype cl-half :uint16)
+(defctype cl-float :float)
+(defctype cl-double :double)
+
+(defctype size-t :uint64)
+(defctype intptr-t :int64)
+(defctype uintptr-t :uint64)
