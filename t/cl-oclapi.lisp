d912f67fa75f9995d60b9b8a07666deabdcf1d92
gos-k <mag4.elan@gmail.com>
2016-05-04 19:29:11 +0900

    add unittest cl-release-event

--- t/cl-oclapi.lisp
+++ t/cl-oclapi.lisp
@@ -471,7 +471,8 @@
                                               (null-pointer)))
     (ok (null-pointer-p (cl-create-user-event (null-pointer)
                                               (null-pointer))))
-    (is +cl-invalid-event+ (cl-retain-event (null-pointer)))))
+    (is +cl-invalid-event+ (cl-retain-event (null-pointer)))
+    (is +cl-invalid-event+ (cl-release-event (null-pointer)))))
 
 (subtest "Flush and Finish API"
   (subtest "can call functions."
