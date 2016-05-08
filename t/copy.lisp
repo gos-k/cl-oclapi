2de243b96da5a6abad88463209d18fd3a9ee0e0f
gos-k <mag4.elan@gmail.com>
2016-05-08 19:23:17 +0900

    fix unittest copy buffer

--- t/copy.lisp
+++ t/copy.lisp
@@ -10,7 +10,7 @@
 (plan nil)
 
 (defparameter *copy-kernel*
-  "__kernel void alfa(__global char * a) { a[0] = 1; }")
+  "__kernel void alfa(__global char * a, __global char * b) { a[0] = b[0]; }")
 
 (subtest "Copy buffer"
   (with-foreign-objects ((devices 'cl-device-id)
@@ -62,8 +62,10 @@
       (let ((context (create-context (null-pointer) 1 devices))
             (device (mem-aref devices 'cl-device-id)))
         (ok context "create context")
-        (let ((buffer (create-buffer context +cl-mem-read-write+ 1000)))
-          (ok buffer "create buffer")
+        (let ((buffer-in (create-buffer context +cl-mem-read-only+ 1))
+              (buffer-out (create-buffer context +cl-mem-write-only+ 1)))
+          (ok buffer-in "create buffer-in")
+          (ok buffer-out "create buffer-out")
           (with-foreign-string (source *copy-kernel*)
             (with-foreign-object (p :pointer)
               (setf (mem-aref p :pointer) source)
@@ -75,11 +77,19 @@
                 (with-foreign-string (name "alfa")
                   (let ((kernel (create-kernel program name)))
                     (ok kernel "create kernel")
-                    (with-foreign-object (p :pointer)
-                      (setf (mem-aref p :pointer) buffer)
-                      (is-success (cl-set-kernel-arg kernel 0 8 p) "set kernel arg")
+                    (with-foreign-objects ((p-in :pointer)
+                                           (p-out :pointer))
+                      (setf (mem-aref p-in :pointer) buffer-in)
+                      (setf (mem-aref p-out :pointer) buffer-out)
+                      (is-success (cl-set-kernel-arg kernel 0 8 p-out) "set kernel arg")
+                      (is-success (cl-set-kernel-arg kernel 1 8 p-in) "set kernel arg")
                       (let ((command-queue (create-command-queue context device 0)))
                         (ok command-queue "create command queue")
+                        (with-foreign-object (value 'cl-char)
+                          (setf (mem-aref value 'cl-char) 1)
+                          (enqueue-write-buffer command-queue buffer-in 1 0 1 value)
+                          (setf (mem-aref value 'cl-char) 2)
+                          (enqueue-write-buffer command-queue buffer-out 1 0 1 value))
                         (with-foreign-objects ((global-work-size 'size-t 3)
                                                (local-work-size 'size-t 3))
                           (set-work-size global-work-size 1)
@@ -93,18 +103,13 @@
                         (is-success (cl-finish command-queue) "finish")
                         (with-foreign-object (value 'cl-char)
                           (setf (mem-aref value 'cl-char) 0)
-                          (enqueue-read-buffer command-queue
-                                               buffer
-                                               0
-                                               0
-                                               1
-                                               value)
-                          (is-success (cl-finish command-queue) "finish")
-                          (is 1 (mem-aref value 'cl-char) "result"))
+                          (enqueue-read-buffer command-queue buffer-out 1 0 1 value)
+                          (is 1 (mem-aref value 'cl-char) "buffer-out result"))
                         (is-success (cl-release-command-queue command-queue) "release command queueu")))
                     (is-success (cl-release-kernel kernel) "release kernel")))
                 (is-success (cl-release-program program) "release program"))))
-          (is-success (cl-release-mem-object buffer) "release mem object"))
+          (is-success (cl-release-mem-object buffer-in) "release mem object")
+          (is-success (cl-release-mem-object buffer-out) "release mem object"))
         (is-success (cl-release-context context) "release context")
         (is-success (cl-release-device device) "release device")))))
 
